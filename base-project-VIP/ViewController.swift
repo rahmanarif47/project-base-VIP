04c7793c31fcbfa06be650bdc2c6d239fa1cc484
rahmanarif47 <rahmanarif.9764@gmail.com>
2022-01-16 10:49:32 +0700

    Initial Commit

---
+++ base-project-VIP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  base-project-VIP
+//
+//  Created by Arif Rahman Sidik on 16/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
